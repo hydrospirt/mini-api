84ce3127ac254db5b8c1bf1cb10147e2b63a7130
hydro <hydrospirt@mail.ru>
2023-11-15 14:21:33 +0300

    Add TextFileViewset

--- mini_drive/api/views.py
+++ mini_drive/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from api.serializers import TextFileSerializer
+from files.models import TextFile
 
-# Create your views here.
+
+class TextFileViewset(viewsets.ModelViewSet):
+    queryset = TextFile.objects.all()
+    serializer_class = TextFileSerializer
