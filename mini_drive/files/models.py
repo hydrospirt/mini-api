511bf389b49b94072a7f3200c9a3c246387f1f18
hydro <hydrospirt@mail.ru>
2023-11-15 13:34:53 +0300

    Add TextFile model

--- mini_drive/files/models.py
+++ mini_drive/files/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class TextFile(models.Model):
+    name = models.CharField(
+        max_length=56,
+        verbose_name='Имя файла',
+        help_text='Добавьте название вашего файла'
+    )
+    data = models.TextField(
+        verbose_name='Содержимое файла'
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата создания',
+        auto_now_add=True
+    )
+
+    class Meta:
+        verbose_name = 'Текстовый файл'
+        verbose_name_plural = 'Текстовые файлы'
+
+    def __str__(self) -> str:
+        return self.name
