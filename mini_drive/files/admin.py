3a7e4f855b5b7568b6addcaf7cb08cbc7fbe696c
hydro <hydrospirt@mail.ru>
2023-11-15 15:10:57 +0300

    Fix list_filter

--- mini_drive/files/admin.py
+++ mini_drive/files/admin.py
@@ -6,5 +6,5 @@ from files.models import TextFile
 class TextFileAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'data')
     search_fields = ('name',)
-    list_filter = ('pub_date')
+    list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
