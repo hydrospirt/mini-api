0508c6698a6b4742d0a296d0912c5cfb413d6939
hydro <hydrospirt@mail.ru>
2023-11-15 13:43:50 +0300

    Add settings for Google API

--- mini_drive/mini_drive/settings.py
+++ mini_drive/mini_drive/settings.py
@@ -129,3 +129,21 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Settings For Google API
+
+EMAIL_USER = os.environ['YOUR_EMAIL']
+
+INFO = {
+    'type': os.environ['TYPE'],
+    'project_id': os.environ['PROJECT_ID'],
+    'private_key_id': os.environ['PRIVATE_KEY_ID'],
+    'private_key': os.environ['PRIVATE_KEY'],
+    'client_email': os.environ['CLIENT_EMAIL'],
+    'client_id': os.environ['CLIENT_ID'],
+    'auth_uri': os.environ['AUTH_URI'],
+    'token_uri': os.environ['TOKEN_URI'],
+    'auth_provider_x509_cert_url': os.environ['AUTH_PROVIDER_X509_CERT_URL'],
+    'client_x509_cert_url': os.environ['CLIENT_X509_CERT_URL']
+}
