43a80aa36c313661f2557dc13e8d060e4aa16319
hydro <hydrospirt@mail.ru>
2023-11-15 13:35:12 +0300

    Add TextFileAdmin

--- mini_drive/files/admin.py
+++ mini_drive/files/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from files.models import TextFile
 
-# Register your models here.
+
+@admin.register(TextFile)
+class TextFileAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'data')
+    search_fields = ('name',)
+    list_filter = ('pub_date')
+    empty_value_display = '-пусто-'
