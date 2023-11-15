df7e4ac6220167b22986a3764976148fa39a4607
hydro <hydrospirt@mail.ru>
2023-11-15 14:20:47 +0300

    Add RegexValidator and unique for name field

--- mini_drive/files/models.py
+++ mini_drive/files/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 
 class TextFile(models.Model):
     name = models.CharField(
         max_length=56,
         verbose_name='Имя файла',
-        help_text='Добавьте название вашего файла'
+        help_text='Добавьте название вашего файла',
+        unique=True,
+        validators=[RegexValidator(regex=r'^[A-Za-zА-Яа-я0-9_]+\.txt$')]
     )
     data = models.TextField(
         verbose_name='Содержимое файла'
